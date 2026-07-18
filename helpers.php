9ed65c6039dfc9de35db19dfd89fdd27775e5efd
Dushesa <andrejmanusa2@gmail.com>
2026-07-18 18:11:41 +0200

    обновили хелперс

--- helpers.php
+++ helpers.php
@@ -42,7 +42,6 @@ function sendMessage(
 
 
 
-
 function sendLongMessage(
     $chat_id,
     $thread_id,
@@ -68,7 +67,6 @@ function sendLongMessage(
     }
 
 }
-
 
 
 
@@ -289,57 +287,4 @@ function addLog(
 
     );
 
-}
-
-
-
-
-// =====================================
-// USER PLAYERS
-// =====================================
-
-function getUserPlayers($telegram_id)
-{
-    return userPlayerRepository()
-        ->getByUser($telegram_id);
-}
-
-
-function getMainPlayer($telegram_id)
-{
-    return userPlayerRepository()
-        ->getMain($telegram_id);
-}
-
-
-function hasUserPlayer($player_tag)
-{
-    return userPlayerRepository()
-        ->exists($player_tag);
-}
-
-
-function addUserPlayer(
-    $telegram_id,
-    $player_tag
-)
-{
-    return userPlayerRepository()
-        ->create(
-            $telegram_id,
-            $player_tag
-        );
-}
-
-
-function setMainPlayer(
-    $telegram_id,
-    $player_tag
-)
-{
-    return userPlayerRepository()
-        ->setMain(
-            $telegram_id,
-            $player_tag
-        );
 }
